6c7111961786aefc9a44523a25a1be339e664adb
AnkitSavani111 <ankitmsavani12345@gmail.com>
2026-09-01 21:22:00 +0530

    onyx: ProjectInfinity-X
    Signed-off-by: AnkitSavani111 <ankitmsavani12345@gmail.com>

--- infinity_onyx.mk
+++ infinity_onyx.mk
@@ -8,12 +8,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
 # Inherit from onyx device
 $(call inherit-product, device/xiaomi/onyx/device.mk)
 
-PRODUCT_NAME := lineage_onyx
+INFINITY_MAINTAINER := "AnkitSavani"
+TARGET_HAS_UDFPS := true
+WITH_GAPPS := true
+
+PRODUCT_NAME := infinity_onyx
 PRODUCT_DEVICE := onyx
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
